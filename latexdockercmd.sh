12bef27b23da432beaaf17052e1dcc65927aa6b0
Viktor <viktor@tiulp.in>
2019-10-14 22:32:31 +0300

    Update latexdockercmd.sh

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-IMAGE=iamm-templates/image:latest
+IMAGE=tiulpin/iamm-templates:latest
+docker pull "$IMAGE"
 exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
