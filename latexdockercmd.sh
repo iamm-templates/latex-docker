dad342612740526b533081ff00539cad7e5171e3
Viktor <viktor@tiulp.in>
2019-10-15 02:07:29 +0300

    Update latexdockercmd.sh

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 IMAGE=tiulpin/xelatex-docker:latest
+wget https://github.com/tiulpin/xelatex-docker/raw/master/.latexmkrc -O .latexmkrc
 docker pull "$IMAGE"
 exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "latexmk -cd -f -interaction=batchmode -pdf"
